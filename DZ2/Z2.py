c6877654d0cdbb100793b06df527d9c759b2fb1b
Ольга <olga_kosareva22@mail.ru>
2022-11-21 20:15:16 +0300

    добавление задачи 2 в DZ2

--- DZ2/Z2.py
+++ DZ2/Z2.py
@@ -1,5 +1,14 @@
 #Напишите программу, 
 #которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
-n = int(input('Введите число N = '))
-list = [1,n]
-print(list)
+def mult(n):
+    if n == 1:
+        return 1
+    else:
+        return n * mult(n - 1)
+
+n = int(input("Введите число: "))
+list = []
+for e in range(1, n + 1):
+    list.append(mult(e))
+
+print(f"Произведения чисел от 1 до {n}:  {list}")
